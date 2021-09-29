23d8d73b8c034e758022c509d431cb77d916ff04
Павел <p_m_smirnov@mail.ru>
2021-09-29 21:40:31 +0300

    добавил еще вызов функции

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function getRandom1(min, max) {
     }
     return Math.floor(Math.random() * (max - min + 1)) + min; 
   }
-
+  getRandom1(0, 100);
 
 function getRandom2(min, max) {
     if ((min < 0) || (min >= max)) {
@@ -14,3 +14,5 @@ function getRandom2(min, max) {
     }
     return Math.random() * (max - min) + min;
   }
+
+  getRandom2(0.25, 99.3);
