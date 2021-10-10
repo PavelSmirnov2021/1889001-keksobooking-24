72a6a85916de02590b3e857119b71d1f4d37a667
Павел <p_m_smirnov@mail.ru>
2021-10-10 13:13:51 +0300

    4.9. Больше деталей

--- js/main.js
+++ js/main.js
@@ -14,6 +14,24 @@ function getRandomFloat(min, max, precision) {
 }
 getRandomFloat(0.5, 98.99, 4);
 
-let author = {};
-let offer = {};
-let location = {};
+const author = {
+  avatar: 'img/avatars/user01.png'
+};
+const offer = {
+  title: 'Бронирование отеля',
+  address: '35.65000, 139.70000',
+  price: price(),
+  type: ['palace', 'flat', 'house', 'bungalow', 'hotel'],
+  rooms: rooms(),
+  guests: guests(),
+  checkin: ['12:00', '13:00', '14:00'],
+  checkout: ['12:00', '13:00', '14:00'],
+  features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+  description: 'Отель с видом на горы',
+  photos: ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg']
+};
+const location = {
+  lat: lat(),
+  lng: lng()
+};
+
